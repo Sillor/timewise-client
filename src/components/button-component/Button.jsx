2c700e4cb4b9a4a127a878d2293a97aa426bc305
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2024-01-09 14:36:41 -0800

    added box shadow

--- src/components/button-component/Button.jsx
+++ src/components/button-component/Button.jsx
@@ -7,7 +7,8 @@ const Button = ({backgroundColor, border, borderRadius, padding, textColor, text
         border: border || 'none',
         borderRadius: borderRadius || '10px',
         padding: padding || '5px 15px',
-        color: textColor || 'white'
+        color: textColor || 'white',
+        boxShadow: '0 2px 4px rgba(0,0,0,0.2)'
     }
   return (
     <button style={buttonStyle}>
