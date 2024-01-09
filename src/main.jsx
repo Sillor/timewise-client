4133c979dcb27b84131e547c3684945af039b86f
AlissonRoss <alisson@alisson.me>
2024-01-09 15:31:34 -0800

    fixed path

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import CreateAccountPage from "./timetracker/components/create-account-page/CreateAccountPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <CreateAccountPage />
+    <App />
   </React.StrictMode>
 );
