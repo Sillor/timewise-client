068abd0c878318f7c2f3a67ab65fe266c9778609
JamenGilbreath <jamen.gilbreath.cohort233@gmail.com>
2024-01-08 16:17:59 -0800

    timesheet main folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Timesheet from './timetracker/Timesheet-main';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello TimeWise!</h1>
+    <div >
+      {/* className="min-h-screen bg-gray-900 text-white flex items-center justify-center" */}
+      {/* <h1 className="text-4xl font-bold">Hello TimeWise!</h1> */}
+    
+          
+       
+        <Timesheet/>
+       
     </div>
+    
   );
 }
 
