58d2194993a6691526e94b66d690cacee917c5e8
Nolan Lawler <nlawler0207@gmail.com>
2024-01-09 15:03:06 -0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello TimeWise!</h1>
-    </div>
+    <>
+      <nav className="relative">
+        <div className="nav--header flex [&>div]:px-4 [&>div]:cursor-pointer">
+          <div>Ham</div>
+          <div>one</div>
+          <div>two</div>
+        </div>
+        <div className="nav--sidebar fixed bg-orange-500 h-full w-40 -translate-x-full">test</div>
+      </nav>
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello TimeWise!</h1>
+      </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, Router, RouterProvider} from "react-router-dom"
 import App from './App.jsx';
+
+const div = <div>this is test</div>
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "test",
+        element: div
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
