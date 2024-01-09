56dca50fb2f97a0a95480c5fa0665019133530e7
Gome510 <bluehawkfire55@gmail.com>
2024-01-08 23:29:56 -0800

    [refactor] fixed font size and time box sizes

--- src/timetracker/MainContent/NewEntryForm.jsx
+++ src/timetracker/MainContent/NewEntryForm.jsx
@@ -16,11 +16,11 @@ function NewEntryForm() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="w-full flex flex-col items-center gap-3 rounded-lg bg-sea-blue bg-opacity-50 p-5 "
+      className="w-full flex flex-col items-center gap-3 rounded-lg bg-sea-blue bg-opacity-50 p-5 text-xs"
     >
       <h2 className="text-3xl font-bold w-full text-left">New Entry</h2>
       <div className="flex gap-2.5 w-full">
-        <div className="flex flex-col w-full gap-2 text-black">
+        <div className="flex flex-col w-full gap-2.5 text-black">
           <input
             required
             className="w-full rounded-md p-2"
@@ -28,10 +28,10 @@ function NewEntryForm() {
             placeholder="Summary"
           />
 
-          <div className="flex gap items-center *:rounded-md *:p-2 ">
+          <div className="flex gap-1 items-center *:rounded-md *:max-h-8 ">
             <input
               required
-              className="sm:max-w-full sm:w-full max-w-28"
+              className="w-28 p-2"
               type={timeType}
               onFocus={() => setTimeType("time")}
               placeholder="Time Start"
@@ -39,7 +39,7 @@ function NewEntryForm() {
             <p className="text-white text-2xl">-</p>
             <input
               required
-              className="sm:max-w-full sm:w-full max-w-28"
+              className="w-28 p-2"
               type={timeType}
               onFocus={() => setTimeType("time")}
               placeholder="Time End"
