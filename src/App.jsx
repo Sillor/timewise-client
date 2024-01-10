67f5cca0bc0f20ffc97eb9fc65ee19541df8f294
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-09 17:25:05 -0800

    getting started with the layout, need to change spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import ForgotPassword from './components/ForgotPassword.jsx';
+import ResetPassword from './components/ResetPassword.jsx';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello TimeWise!</h1>
+    <div className='app'>
+      {/*className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello TimeWise!</h1>*/}
+      <ForgotPassword />
+      <hr />
+      <ResetPassword />
     </div>
   );
 }
---
+++ src/components/ForgotPassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function ForgotPassword() {
+
+    return (
+        <h1>Forgot your password?</h1>
+    );
+}
+
+
+export default ForgotPassword;
