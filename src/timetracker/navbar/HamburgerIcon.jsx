51a56997a199c5e08cc307a75de5fcba63c00980
Nolan Lawler <nlawler0207@gmail.com>
2024-01-10 14:37:15 -0800

    added icons to "icons" folder

--- src/timetracker/navbar/HamburgerIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HamburgerIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="" height="" fill="currentColor" viewBox="0 0 16 16">
-  <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-</svg>
-  )
-}
