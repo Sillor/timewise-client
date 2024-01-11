a24a3a9392ccd236cbcbf5ddaa88f638f93cb6dc
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-10 17:37:32 -0800

    check is password is valid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import './components/ConfirmPasswordReset.css'
+import ConfirmPasswordReset from './components/ConfirmPasswordReset';
 
 
 function App() {
   return (
     <>
-
+      <ConfirmPasswordReset />
     </>
   );
 }
--- src/components/ConfirmPasswordReset.jsx
+++ src/components/ConfirmPasswordReset.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ConfirmPasswordReset.css';
 
 function ConfirmPasswordReset() {
+    const [password, setPassword] = useState('');
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const isPasswordValid = () => {
+        if (password.length < 12) {
+            return false;
+        }
+
+        const specialCharacter = /[!@#$%^&*(),.?":{}|<>]/;
+        if (!specialCharacter.test(password)) {
+            return false;
+        }
+        const forbiddenStrings = ["password", "123", "1234", "12345", "123456"];
+        if (forbiddenStrings.some(forbiddenString => password.includes(forbiddenString))) {
+            return false;
+        }
+        return true;
+    };
 
     return (
         <div className="app flex justify-center items-center h-screen">
@@ -10,32 +31,36 @@ function ConfirmPasswordReset() {
             <div>
                 <h2 className="text-5xl mb-9">Reset<br /> Password</h2>
                 <form>
-                    <label>
+                    <label className="text-black">
                         <input
-                            type="text"
+                            type="password"
                             placeholder="New Password"
-                            className="password-input"
+                            className="password-input p-2 w-full"
+                            value={password}
+                            onChange={handlePasswordChange}
                         />
                     </label>
-                    <label>
+                    <label className="text-black mb-3">
                         <input
-                            type="text"
+                            type="password"
                             placeholder="Confirm Password"
-                            className="password-input"
+                            className="password-input p-2 w-full h-9"
                         />
                     </label>
-                    {/* 
-                <p className="password-req">
-                    Password must be at least 12 characters and must include a special character.
-                    Passwords cannot include any of the following strings:
-                    “password”
-                    “123”
-                    “1234”
-                    “12345”
-                    “123456”
-                </p>
-                */}
-                    <button type="submit" className="submit-button mt-4">Confirm</button>
+                    {isPasswordValid() ? null : (
+                        <p className="password-req text-red-500 mb-3">
+                            Password must be at least 12<br /> characters and must include a special<br /> character.
+                            Passwords cannot include<br /> any of the following strings:<br />
+                            <ul className="list-disc list-inside">
+                                <li>“password”</li>
+                                <li>“123”</li>
+                                <li>“1234”</li>
+                                <li>“12345”</li>
+                                <li>“123456”</li>
+                            </ul>
+                        </p>
+                    )}
+                    <button type="submit" className="submit-button w-45">Confirm</button>
                     <h5>New user?</h5>
                     <a href="./SignUp">Sign Up</a>
                 </form>
