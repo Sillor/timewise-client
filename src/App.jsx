cb53b9a4cf6482b20b78d768cf0553bd3a03c695
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-12 13:55:43 -0800

    remove confirm password reset component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import './components/ConfirmPasswordReset.css'
-import ConfirmPasswordReset from './components/ConfirmPasswordReset';
-
 
 function App() {
   return (
     <>
-      <ConfirmPasswordReset />
     </>
   );
 }
