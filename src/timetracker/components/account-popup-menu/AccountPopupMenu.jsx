308f4c728974c90ba8d483a9effb79c9f4aa9784
Nolan Lawler <nlawler0207@gmail.com>
2024-01-16 13:34:42 -0800

    fixed popup open on page load

--- src/timetracker/components/account-popup-menu/AccountPopupMenu.jsx
+++ src/timetracker/components/account-popup-menu/AccountPopupMenu.jsx
@@ -12,7 +12,7 @@ export default function AccountPopupMenu({isOpen, setIsOpen}) {
     <div
       className={`bg-dark text-white text-xl
         fixed flex flex-col transition pt-5 z-10 right-0 box-border
-        shadow-dark ${!isOpen ? "translate-x-[calc(100%+1px)]" : "shadow-lg"}`}
+        shadow-dark ${isOpen ? "translate-x-[calc(100%+1px)]" : "shadow-lg"}`}
     >
       <div className='text-sm px-10 pb-5 text-zinc-300'>email@example.com</div>
       {/* <Link to="/account" className='flex items-center px-10 text-base py-2 cursor-pointer hover:bg-zinc-700 focus:bg-zinc-700'>Account Settings</Link> */}
