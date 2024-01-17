dbae17faaac40274a24ee1bff9fcf80751cdb348
Nolan Lawler <nlawler0207@gmail.com>
2024-01-16 18:25:24 -0800

    added navbar close on blur

--- src/timetracker/components/navbar/Sidebar.jsx
+++ src/timetracker/components/navbar/Sidebar.jsx
@@ -1,46 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 import ProjectsIcon from "../../assets/ProjectsIcon.jsx";
 import ClockIcon from "../../assets/ClockIcon.jsx";
 import SidebarLink from "./SidebarLink.jsx";
 import UsersIcon from "../../assets/UsersIcon.jsx";
 
-export default function Sidebar({ isOpen, setIsOpen }) {
+export default function Sidebar({ focusElement, setBlurredAt }) {
+  const sidebarElement = useRef(null);
   const links = [
     { to: "/tracker", name: "Time Tracker", icon: <ClockIcon /> },
     { to: "/projects", name: "Projects", icon: <ProjectsIcon /> },
     { to: "/users", name: "Users", icon: <UsersIcon /> },
-    { to: "/timesheet", name: "Timesheet" },
+    { to: "/users", name: "Timesheet" },
     { to: "/register", name: "Register" },
     { to: "/login", name: "Login" },
     { to: "/resetpassword", name: "Reset Password" },
   ].map((link, i) => (
-    <SidebarLink
-      key={i}
-      to={link.to}
-      name={link.name}
-      icon={link.icon}
-      isSidebarOpen={isOpen}
-      closeSidebarFunc={() => setIsOpen(false)}
-    />
+    <SidebarLink key={i} to={link.to} name={link.name} icon={link.icon} />
   ));
-
+  function handleOnBlur() {
+    setBlurredAt(Date.now());
+  }
   return (
     <div
+      onBlur={handleOnBlur}
+      ref={sidebarElement}
       className={`nav--sidebar bg-dark text-white text-xl h-[calc(100vh-64px)]
         fixed overflow-y-auto flex flex-col transition pt-3 z-10
         shadow-dark border-r-[1px] border-gray-500 pe-10
-        [&>*]:py-3 [&>*]:ps-5
-        ${!isOpen ? "-translate-x-full" : "shadow-2xl"}`}
+        [&>:not([aria-hidden])]:py-3 [&>:not([aria-hidden])]:ps-5 
+        [&:has(>:focus)]:shadow-2xl [&:not(:has(>:focus))]:-translate-x-full
+        `}
     >
+      <a aria-hidden ref={focusElement} tabIndex={-1}></a>
       {links}
     </div>
   );
 }
 
 Sidebar.propTypes = {
-  isOpen: PropTypes.bool,
-  setIsOpen: PropTypes.func,
+  focusElement: PropTypes.object,
+  isOpen: PropTypes.object,
+  setBlurredAt: PropTypes.func,
 };
