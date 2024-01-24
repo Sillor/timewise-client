6c004aedb05573f0980fea3b6bba08c6296336da
Goose1771 <gustavo.acosta02@outlook.com>
2024-01-24 14:22:25 -0800

    fixed eye password pt2

--- src/timetracker/pages/login/SignIn.jsx
+++ src/timetracker/pages/login/SignIn.jsx
@@ -7,12 +7,19 @@ function SignIn() {
     return (
       <div className='text-center'>
         <h1 className='text-4xl mb-8 lg:text-6xl lg:mb-10'>Sign In</h1>
-        <input className='text-black rounded-md pr-12 pl-3 py-1 mb-3 lg:rounded-md lg:pr-12 lg:pl-3 lg:py-1 lg:mb-2.5' type='email' placeholder='Email address'/>
-        <br></br>
-        <PasswordInput />
-        <h2> <a className='text-xs lg:text-xs text-sky-500 underline pl-28 lg:pl-28' href='_blank'> Forgot your password?</a></h2>
-        <button className='bg-amber-500 rounded-md px-24 py-2.5 mt-5 mb-1.5'>Sign In</button>
-        <h2 className='mt-2.5'>New user? <a className='text-sky-500 underline' href='_blank'>Sign up</a></h2>
+        <form>
+        <FormInput
+            type="email"
+            placeholder="Email Address"
+          />
+        <FormInput
+            type="password"
+            placeholder="Email Address"
+          />
+        <h2> <a className='text-xs lg:text-xs text-sky-500 underline pl-28 lg:pl-28' href='/resetpassword'> Forgot your password?</a></h2>
+        <button className='bg-amber-500 rounded-md px-24 py-2.5 mt-3 mb-1.5'>Sign In</button>
+        </form>
+        <h2 className='mt-2.5'>New user? <a className='text-sky-500 underline' href='/register'>Sign up</a></h2>
       </div>
     );
   }
