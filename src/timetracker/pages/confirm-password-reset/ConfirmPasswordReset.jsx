55ff784e79308654b74d562435f14e993dba9a00
Egor <65542192+Sillor@users.noreply.github.com>
2024-01-25 13:26:45 -0800

    Fixes after another merge

--- src/timetracker/pages/confirm-password-reset/ConfirmPasswordReset.jsx
+++ src/timetracker/pages/confirm-password-reset/ConfirmPasswordReset.jsx
@@ -6,8 +6,6 @@ import Button from '../../components/button-component/Button.jsx';
 import Greeting from '../../components/greeting/Greeting.jsx';
 
 function ConfirmPasswordReset() {
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
   const [token, setToken] = useState('');
   const [message, setMessage] = useState({ text: '', success: false });
 
@@ -59,11 +57,22 @@ function ConfirmPasswordReset() {
     <div className="flex items-center justify-center flex-col lg:flex-row lg:justify-evenly lg:pt-30">
       <Greeting start="Forgot Your " highlight="Password" end="?" />
       <div>
-      <h2 className="text-5xl mb-9 font-bold justify-center text-center">Reset<br /> Password</h2>
-        <form className="flex flex-col gap-4 max-w-md w-full mx-auto" onSubmit={handleSubmit}>
-                    <PasswordResetForm inputData={inputData} setInputData={setInputData}/>
-                    <Button type="submit" className="w-45 h-12 mt-4">Confirm</Button>
-                    <p
+        <h2 className="text-5xl mb-9 font-bold justify-center text-center">
+          Reset
+          <br /> Password
+        </h2>
+        <form
+          className="flex flex-col gap-4 max-w-md w-full mx-auto"
+          onSubmit={handleSubmit}
+        >
+          <PasswordResetForm
+            inputData={inputData}
+            setInputData={setInputData}
+          />
+          <Button type="submit" className="w-45 h-12 mt-4">
+            Confirm
+          </Button>
+          <p
             className={
               'text-center ' +
               (message.success ? 'text-green-500' : 'text-red-500')
@@ -72,9 +81,11 @@ function ConfirmPasswordReset() {
             {message.text}
           </p>
           <div className="flex justify-center">
-          <div className="flex justify-center">
-                        <h5>New user? </h5>
-                        <Link to="/register" className="ml-1 text-primary underline"> Sign Up</Link>
+            <h5>New user? </h5>
+            <Link to="/register" className="ml-1 text-primary underline">
+              {' '}
+              Sign Up
+            </Link>
           </div>
         </form>
       </div>
